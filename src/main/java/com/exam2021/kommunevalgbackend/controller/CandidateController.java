67af39f8b42bb0fd0577ba4f4ea6140e06949792
Geowall13 <rasmushr13@hotmail.com>
2021-12-20 12:37:47 +0100

    Post new candidate implemented

--- src/main/java/com/exam2021/kommunevalgbackend/controller/CandidateController.java
+++ src/main/java/com/exam2021/kommunevalgbackend/controller/CandidateController.java
@@ -2,22 +2,45 @@ package com.exam2021.kommunevalgbackend.controller;
 
 
 import com.exam2021.kommunevalgbackend.model.Candidate;
+import com.exam2021.kommunevalgbackend.model.Party;
+import com.exam2021.kommunevalgbackend.repository.CandidateRepository;
+import com.exam2021.kommunevalgbackend.repository.PartyRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
 public class CandidateController {
 
-    @GetMapping("/candidate/{id}")
-    public ResponseEntity<Candidate> getCandidateFromId(@PathVariable int id){
+    CandidateRepository candidateRepository;
 
-        Candidate candidate = new Candidate();
-        candidate.setId(1);
+    PartyRepository partyRepository;
+
+    public CandidateController(CandidateRepository candidateRepository, PartyRepository partyRepository){
+        this.candidateRepository = candidateRepository;
+        this.partyRepository = partyRepository;
+    }
+
+    @GetMapping("/candidate/{id}")
+    public ResponseEntity<Candidate> getCandidateFromId(@PathVariable long id){
+        Candidate candidate = candidateRepository.findById(id).get();
         return new ResponseEntity<>(candidate, HttpStatus.OK);
+    }
+
+    @GetMapping("/candidates")
+    public ResponseEntity<List<Candidate>> getAllCandidates(){
+        List<Candidate> candidates = candidateRepository.findAll();
+        return new ResponseEntity<>(candidates, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/candidate/{id}", consumes = "application/json")
+    public ResponseEntity<Candidate> createCandidate(@RequestBody Candidate candidate, @PathVariable long id){
+        Party party = partyRepository.findById(id).get();
+        candidate.setParty(party);
+        candidateRepository.save(candidate);
+        return new ResponseEntity<>(candidate, HttpStatus.CREATED);
     }
 }
