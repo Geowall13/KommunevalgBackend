d71b6149a40c59faaf25f3bc5aff35c351aaae4b
Geowall13 <rasmushr13@hotmail.com>
2021-12-20 10:02:11 +0100

    Init

---
+++ src/test/java/com/exam2021/kommunevalgbackend/KommunevalgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam2021.kommunevalgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KommunevalgBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
