d840682082b7ca1cd010b8edde67e607bf2cbf62
Geowall13 <rasmushr13@hotmail.com>
2021-12-20 11:21:51 +0100

    data.sql now has correct syntax...

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,47 +9,47 @@ INSERT INTO party(name, initials) VALUES("Enhedslisten + De Rød Grønne", "Ø")
 
 
 /* Kandidater for Socialdemokraterne */
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Marcel", null, "Meijer", 1);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Michael", null, "Kristensen", 1);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Helle", null, "Hansen", 1);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Karina", null, "Knobelauch", 1);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Stefan", "Hafstein", "Wolffbrandt", 1);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Robert", "V.", "Rasmussen", 1);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Pia", null, "Ramsing", 1);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Anders", "Baun", "Sørensen", 1);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Marcel", null, "Meijer", 1);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Michael", null, "Kristensen", 1);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Helle", null, "Hansen", 1);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Karina", null, "Knobelauch", 1);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Stefan", "Hafstein", "Wolffbrandt", 1);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Robert", "V.", "Rasmussen", 1);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Pia", null, "Ramsing", 1);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Anders", "Baun", "Sørensen", 1);
 
 /* Kandidater for Konservative */
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Per", "Urban", "Olsen", 2);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Peter", null, "Askjær", 2);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Martin", null, "Sørensen", 2);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Louise", null, "Bramstorp", 2);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Sigfred", null, "Jensen", 2);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Jørn", "C.", "Nissen", 2);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Morten", "Ø.", "Kristensen", 2);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Susanne", null, "Andersen", 2);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Iulian", "V.", "Paiu", 2);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Per", null, "Hingel", 2);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Per", "Urban", "Olsen", 2);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Peter", null, "Askjær", 2);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Martin", null, "Sørensen", 2);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Louise", null, "Bramstorp", 2);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Sigfred", null, "Jensen", 2);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Jørn", "C.", "Nissen", 2);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Morten", "Ø.", "Kristensen", 2);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Susanne", null, "Andersen", 2);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Iulian", "V.", "Paiu", 2);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Per", null, "Hingel", 2);
 
 /* Kandidater for Socialistisk Folkeparti */
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Ulla", null, "Holm", 3);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Kjeld", null, "Bønkel", 3);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Anne", "Grethe", "Olsen", 3);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Lone", null, "Krag", 3);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Børge", "S.", "Buur", 3);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Ulla", null, "Holm", 3);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Kjeld", null, "Bønkel", 3);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Anne", "Grethe", "Olsen", 3);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Lone", null, "Krag", 3);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Børge", "S.", "Buur", 3);
 
 /* Kandidater for Dansk Folkeparti */
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Per", null, "Mortensen", 4);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Per", null, "Mortensen", 4);
 
 /* Kandidater for Venstre */
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Søren", null, "Wiese", 5);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Anita", "Elgaard Højholt", "Olesen", 5);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Carsten", null, "Bruun", 5);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Mogens", null, "Exner", 5);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Anja", null, "Guldborg", 5);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Klaus", null, "Holdorf", 5);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Søren", null, "Wiese", 5);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Anita", "Elgaard Højholt", "Olesen", 5);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Carsten", null, "Bruun", 5);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Mogens", null, "Exner", 5);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Anja", null, "Guldborg", 5);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Klaus", null, "Holdorf", 5);
 
 /* Kandidater for Enhedslisten */
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Katrine", "Høegh Mc", "Quaid", 6);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Jette", "M.", "Søgaard", 6);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Søren", null, "Caspersen", 6);
-Insert INTO candidate(firstName, middleName, surName, partyId) VALUES("Pia", null, "Birkmand", 6);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Katrine", "Høegh Mc", "Quaid", 6);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Jette", "M.", "Søgaard", 6);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Søren", null, "Caspersen", 6);
+Insert INTO candidate(first_name, middle_name, sur_name, party) VALUES("Pia", null, "Birkmand", 6);
