43b42f62de2b508d60f29d7cd157b808bcaf57ed
Geowall13 <rasmushr13@hotmail.com>
2021-12-20 13:37:33 +0100

    PartyController with method to return all candidates from a specific party

---
+++ src/main/java/com/exam2021/kommunevalgbackend/controller/PartyController.java
@@ -0,0 +1,42 @@
+package com.exam2021.kommunevalgbackend.controller;
+
+import com.exam2021.kommunevalgbackend.model.Candidate;
+import com.exam2021.kommunevalgbackend.repository.CandidateRepository;
+import com.exam2021.kommunevalgbackend.repository.PartyRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class PartyController {
+
+    PartyRepository partyRepository;
+
+    CandidateRepository candidateRepository;
+
+    public PartyController(PartyRepository partyRepository, CandidateRepository candidateRepository){
+        this.partyRepository = partyRepository;
+        this.candidateRepository = candidateRepository;
+    }
+
+
+    @GetMapping("/party/{id}/candidates/")
+    public ResponseEntity<List<Candidate>> getPartyCandidates(@PathVariable long id){
+
+        List<Candidate> allCandidates = candidateRepository.findAll();
+        List<Candidate> returnList = new ArrayList<>();
+        for(Candidate candidate : allCandidates){
+            if(candidate.getParty().getId() == id){
+                returnList.add(candidate);
+            }
+        }
+        return new ResponseEntity<>(returnList, HttpStatus.OK);
+    }
+
+}
